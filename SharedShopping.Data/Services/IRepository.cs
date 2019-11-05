7dc251ec1cb9952c9a8b53e82478e67ba2bdef6a
hossmi <hossmi@gmail.com>
2019-11-05 22:18:57 +0100

    reorganize IRepository

--- SharedShopping.Data/Services/IRepository.cs
+++ SharedShopping.Data/Services/IRepository.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 
 namespace SharedShopping.Data.Services
 {
+    public interface IExpenseRepository
+    {
+
+    }
     public interface IRepository
     {
-        void create(ExpenseData itemData);
-        void save(ExpenseData dataItem);
-        IEnumerable<ExpenseData> getExpensesByTag(int tagId);
-        IEnumerable<ExpenseData> getExpensesByUser(int id);
+        void create(ExpenseData expense);
+        void save(ExpenseData expense);
         void setDebtor(int expenseId, int userId);
         void setExpenseTag(int expenseId, string tagName);
-        
-        void create(UserData dataItem);
-        void save(UserData dataItem);
         IEnumerable<UserData> getDebtorsByExpense(int expenseId);
+        IEnumerable<ContributionData> getContributionsByExpense(int expenseId);
+        void setContribution(ContributionData contribution);
+        IEnumerable<TagData> getTagsByExpense(int expenseId);
+        
+        void create(UserData user);
+        void save(UserData user);
+        IEnumerable<ExpenseData> getExpensesByUser(int userId);
+        IEnumerable<ContributionData> getContributionsByUser(int userId);
         UserData getSingleUser(string userName);
         UserData getSingleUser(int userId);
         
-        void create(ContributionData dataItem);
-        void save(ContributionData dataItem);
-        IEnumerable<ContributionData> getContributionsByExpense(int expenseId);
-        IEnumerable<ContributionData> getContributionsByUser(int id);
-        void setContribution(ContributionData contributionData);
-
         void saveOrCreate(TagData tag);
         TagData getOrCreateTag(string name);
-        IEnumerable<TagData> getTagsByExpense(int id);
+        IEnumerable<ExpenseData> getExpensesByTag(int tagId);
         IEnumerable<TagData> getTags();
     }
 }
