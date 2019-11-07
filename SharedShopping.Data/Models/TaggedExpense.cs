12bc4b81f1fdcd116649efb0598f73a8ce7475a7
Pablo <pablo.jimena.everis@outlook.es>
2019-11-07 11:38:37 +0100

    add DebtorData and TaggedExpense as many-to-many entities

---
+++ SharedShopping.Data/Models/TaggedExpense.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedShopping.Data.Models
+{
+    public class TaggedExpense : AbstractData
+    {
+        public int TagId { get; set; }
+        public int ExpenseId { get; set; }
+    }
+}
