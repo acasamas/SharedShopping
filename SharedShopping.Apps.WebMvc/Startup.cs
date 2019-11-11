fbd0eb46820003d4a508778313e4b401e5865d54
Pablo <pablo.jimena.everis@outlook.es>
2019-11-11 14:51:23 +0100

    add mvc project and its test project

---
+++ SharedShopping.Apps.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharedShopping.Apps.WebMvc.Startup))]
+namespace SharedShopping.Apps.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
